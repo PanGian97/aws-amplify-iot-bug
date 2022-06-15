d35ea57d545f9d652d5fc4b08b4312da64ebac5d
pangian <pangianmain@gmail.com>
2022-06-15 22:03:38 +0300

    git commit -m "commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { Authenticator } from '@aws-amplify/ui-react';
+import { AWSIoTProvider } from '@aws-amplify/pubsub';
+import { useEffect } from 'react';
+import '@aws-amplify/ui-react/styles.css';
+import Amplify, { Auth,PubSub } from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+
+
 
 function App() {
+  
+useEffect(()=>{
+  PubSub.addPluggable(new AWSIoTProvider({
+    aws_pubsub_region: 'us-east-2',
+    aws_pubsub_endpoint: 'wss://al3qi2b1mxp67-ats.iot.us-east-2.amazonaws.com/mqtt',
+}));
+    console.log('called')
+    let subscription  =PubSub.subscribe('bug').subscribe({
+        next: data => console.log('Message received', data),
+        error: error => console.error(error),
+        complete: () => console.log('Done'),
+    });
+    return () => {
+
+        subscription.unsubscribe('bug')
+      } 
+},[])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+    {({ signOut, user }) => (
+      <main>
+       
+        <button onClick={signOut}>Sign out</button>
+      </main>
+    )}
+  </Authenticator>
   );
 }
 
